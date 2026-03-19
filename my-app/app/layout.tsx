a77a16b9a08cfea6997ef697664d5d7ba0b2ddc3
Amar <amarrafii369@gmail.com>
2026-03-19 13:12:02 +0800

    feat:(adding vercel speed and analytics insigh )

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,6 +3,8 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -57,6 +59,8 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
+        <SpeedInsights />
         <VisualEditing />
       </body>
     </html>
